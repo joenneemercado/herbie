a8bdfaef8af0dec81b652fad35a5603ed5a4256d
Douglas Santos <douglas.santos@emercadoapp.com.br>
2025-05-22 17:49:09 -0400

    feat: refatorar UpdateCampaignDto para incluir validações e propriedades detalhadas

--- src/campaigns/dto/update-campaign.dto.ts
+++ src/campaigns/dto/update-campaign.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCampaingDto } from './create-campaign.dto';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCampaignDto extends PartialType(CreateCampaingDto) {}
+export class UpdateCampaignDto {
+  @IsNumber()
+  @ApiProperty({ description: 'ID da campanha', example: 1 })
+  id: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty({ description: 'Prioridade da campanha', example: 1 })
+  priority: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty({ description: 'Status do campanha', example: 1 })
+  status_id: number;
+
+  @IsString({ message: 'O organization_id deve ser um UUID válido' })
+  @ApiProperty({ example: 'cm0l1u61r00003b6junq2pmbi' })
+  organization_id: string;
+}
